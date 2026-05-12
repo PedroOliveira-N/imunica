98e04c5ee40ba1d58306186ce94c4f7e452370b9
PedroLucas-O <pedrolucas.pc.cp@gmail.com>
2026-05-11 21:07:17 -0300

    iniciando transição entre cards horizontais para cards verticais

--- src/pages/EducativaPage/index.jsx
+++ src/pages/EducativaPage/index.jsx
@@ -1,58 +1,79 @@
 import styles from "./styles.module.css";
-import { Button, Card, Image, Text } from "@chakra-ui/react";
+import { Button, Card, Image, SimpleGrid, Text } from "@chakra-ui/react";
 
 export default function EducativaPage() {
   const alerta = () => {
     alert("Esta Função Ainda em Desenvolvimento!");
-  }
+  };
 
   return (
     <div>
       <div className={styles.centerfied}>
         <h1 className={styles.h1}>Área Educativa</h1>
-        <p>Nesta página você encontrará o catálogo de ferramentas educativas pensadas para desenvolver uma espécie de "imunidade" sobre a desinformação.</p>
+        <p>
+          Nesta página você encontrará o catálogo de ferramentas educativas
+          pensadas para desenvolver uma espécie de "imunidade" sobre a
+          desinformação.
+        </p>
       </div>
 
       <div className={styles.cards}>
-        <Card.Root maxW="50%" overflow="hidden">
-          <Image
-            className={styles.img}
-            src="/image.games.png"
-            alt="jogos"
-          />
-          <Card.Body gap="2">
-            <Card.Title>Jogos Educativos</Card.Title>
-            <Card.Description>
-              Saiba mais sobre a desinformação digital através de jogos e
-              atividades interativas!
-            </Card.Description>
-          </Card.Body>
-          <Card.Footer gap="2">
-            <Button className={styles.linkButton} onClick={alerta}>
-              Lista de Jogos
-            </Button>
-          </Card.Footer>
-        </Card.Root>
+        <SimpleGrid
+          columns={{
+            base: 1,
+            sm: 1,
+            md: 2,
+          }}
+          spacing={{
+            base: 4,
+            md: 6,
+            lg: 8,
+          }}
+          width="100%"
+          justifyItems="center"
+        >
+          <Card.Root 
+            className={styles.cardHorizontal}
+            overflow="hidden"
+          >
+            <Image className={styles.img} src="/image.games.png" alt="jogos" objectFit='cover'/>
+            <Card.Body className={styles.cardContent} gap="2">
+              <Card.Title>Jogos Educativos</Card.Title>
+              <Card.Description>
+                Saiba mais sobre a desinformação digital através de jogos e
+                atividades interativas!
+              </Card.Description>
+            </Card.Body>
+            <Card.Footer gap="2">
+              <Button className={styles.linkButton} onClick={alerta}>
+                Lista de Jogos
+              </Button>
+            </Card.Footer>
+          </Card.Root>
 
-        <Card.Root maxW="50%" overflow="hidden">
-          <Image
-            className={styles.img}
-            src="/image.reading.png"
-            alt="leitura"
-          />
-          <Card.Body gap="2">
-            <Card.Title>Material de Leitura</Card.Title>
-            <Card.Description>
-              Saiba mais sobre a desinformação digital através de conteúdo
-              informativo!
-            </Card.Description>
-          </Card.Body>
-          <Card.Footer gap="2">
-            <Button className={styles.linkButton} onClick={alerta}>
-              Lista de Conteúdos
-            </Button>
-          </Card.Footer>
-        </Card.Root>
+          <Card.Root 
+            className={styles.cardHorizontal}
+            overflow="hidden"
+          >
+            <Image
+              className={styles.img}
+              src="/image.reading.png"
+              alt="leitura"
+            />
+            <Card.Body gap="2">
+              <Card.Title>Material de Leitura</Card.Title>
+              <Card.Description>
+                Saiba mais sobre a desinformação digital através de conteúdo
+                informativo!
+              </Card.Description>
+            </Card.Body>
+            <Card.Footer gap="2">
+              <Button className={styles.linkButton} onClick={alerta}>
+                Lista de Conteúdos
+              </Button>
+            </Card.Footer>
+          </Card.Root>
+        </SimpleGrid>
       </div>
     </div>
   );
